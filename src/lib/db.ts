0fa295f4936f558c310dbda2585c2ddbf6074a04
Devsethi3 <devsethi4522@gmail.com>
2024-11-20 20:56:10 +0530

    Database & Prisma setup

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,22 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
 
-let prisma: PrismaClient;
+export const db = globalThis.prisma || new PrismaClient();
 
-// if (process.env.NODE_ENV !== "production") prisma = new PrismaClient();
-// else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient();
-//   }
-//   prisma = global.prisma;
-// }
-
-if (!global.prisma) {
-  global.prisma = new PrismaClient();
-}
-prisma = global.prisma;
-
-export const db = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
