33dc2363533f08beca723abb142338cfd493af5b
Devsethi3 <devsethi4522@gmail.com>
2024-11-20 21:06:34 +0530

    Creating User

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,12 +2,28 @@
 
 import { RegisterSchema } from "@/schemas";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: "Invalid fields!" };
+  }
+
+  const { name, email, password } = validatedFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in user" };
   }
 
   return { success: "Email Sent!" };
