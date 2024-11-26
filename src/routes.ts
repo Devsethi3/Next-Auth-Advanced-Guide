d24eb764a90dce13f4fdaadb45b44da063c8a214
Dev Prasad Sethi <147725410+Devsethi3@users.noreply.github.com>
2024-11-26 22:18:58 +0530

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -9,7 +9,7 @@ export const publicRoutes = ["/"];
 /**
  * An array of routes that are used for authentication.
  * These routes will redirect logged in users to the default login redirect.
- * @type {string[]}
+ * @type
  */
 
 export const authRoutes = ["/login", "/register", "/error"];
